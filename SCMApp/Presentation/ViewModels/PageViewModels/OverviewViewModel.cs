37b3a98a92f207a8c905854d905d76819d285c67
sonhungel <17521310@gm.uit.edu.vn>
2021-12-14 00:58:32 +0700

    Done overview UI

--- SCMApp/Presentation/ViewModels/PageViewModels/OverviewViewModel.cs
+++ SCMApp/Presentation/ViewModels/PageViewModels/OverviewViewModel.cs
@@ -1,4 +1,6 @@
-﻿using SCMApp.Constants;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using SCMApp.Constants;
 using SCMApp.Presentation.ViewModels.Base;
 using SCMApp.ViewManager;
 using System;
@@ -9,11 +11,34 @@ namespace SCMApp.Presentation.ViewModels.PageViewModels
     {
         public OverviewViewModel(IScreenManager screenManager) : base(screenManager)
         {
+            SeriesCollection = new SeriesCollection()
+            {
+                new ColumnSeries()
+                {
+                    Title = "Thu",
+                    Values = new ChartValues<double> {5,10,15,20,25,30,35}
+                }
+            };
 
+            SeriesCollection.Add(new ColumnSeries()
+            {
+                Title = "Chi",
+                Values = new ChartValues<double> { 3, 7, 13, 19,21,27,34 }
+            });
+
+            BarLabels = new[] { "Thứ 2", "Thứ 3", "Thứ 4", "Thứ 5", "Thứ 6", "Thứ 7", "Chủ Nhật" };
+
+            Formatter = value => value.ToString("N");
         }
         public string NamePage => CommonConstants.OverviewPageViewName;
 
         public string FunctionName => CommonConstants.OverviewFunctionName;
+
+        public SeriesCollection SeriesCollection { get; set; }
+
+        public string[] BarLabels { get; set; }
+
+        public Func<double,string> Formatter { get; set; }
 
         public void Construct()
         {
